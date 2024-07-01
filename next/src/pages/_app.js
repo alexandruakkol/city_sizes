cb8112657daebf4f14eb4884f8aae7777ac54c35
alexandruakkol <alexandru.akkol@gmail.com>
2024-07-01 16:08:44 -0700

    analytics env fix

--- next/src/pages/_app.js
+++ next/src/pages/_app.js
@@ -2,8 +2,11 @@ import './App.css';
 import './index.css';
 import App from './index.js';
 import React from 'react';
-
+import RootLayout from './layout.js';
 
 export default function MyApp({ Component, pageProps }) {
-    return <App {...pageProps} />;
+    
+    return <RootLayout>
+        <App {...pageProps} />
+    </RootLayout>
 }
--- next/src/pages/layout.js
+++ next/src/pages/layout.js
@@ -1,23 +1,24 @@
 import Script from 'next/script';
 
-export default function RootLayout(children) {
+export default function RootLayout({children}) {
+  const gtag = process.env.NEXT_PUBLIC_REACT_GANALYTICS_ID;
   return (
-    <html lang="en">
+    <>
       <Script
         strategy='lazyOnload'
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.REACT_GANALYTICS_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag}`}
       />
       <Script id='' strategy='lazyOnload'>
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${process.env.REACT_GANALYTICS_ID}', {
+          gtag('config', '${gtag}', {
           page_path: window.location.pathname,
           });
         `}
       </Script>    
-      <body>{children}</body>
-    </html>
+      <>{children}</>
+    </>
   )
 } 
