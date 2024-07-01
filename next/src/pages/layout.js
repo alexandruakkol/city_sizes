9df65d5eefba4590f2c1bfc3714ae58bc52ef3fd
alexandruakkol <alexandru.akkol@gmail.com>
2024-07-01 12:26:13 -0700

    g analytics fix

--- next/src/pages/layout.js
+++ next/src/pages/layout.js
@@ -1,10 +1,23 @@
-import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script';
 
 export default function RootLayout(children) {
   return (
     <html lang="en">
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.REACT_GANALYTICS_ID}`}
+      />
+      <Script id='' strategy='lazyOnload'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.REACT_GANALYTICS_ID}', {
+          page_path: window.location.pathname,
+          });
+        `}
+      </Script>    
       <body>{children}</body>
-      <GoogleAnalytics gaId="G-CYVR2V7CTR" />
     </html>
   )
 } 
